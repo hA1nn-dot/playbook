c8b14480b534853df0ab70eed842b36c41e321c9
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-04-16 13:59:50 -0500

    Complete examples_1

---
+++ weekly_mission_2/examples_1/15_ejemplo_sort.js
@@ -0,0 +1,4 @@
+//Ejemplo 15: Uso de sort para ordenar elementos alfabeticamente
+const products = ["Milk", "Coffee", "Sugar", "Honey", "Apple", "Carrot"];
+console.log("Ejemplo 15: Elementos ordernados usando SORT");
+console.log(products.sort());
---
+++ weekly_mission_2/examples_1/8_ejemplo_listaCondicional.js
@@ -0,0 +1,14 @@
+// Ejemplo 8: Filtrar una lista por condicional
+//Lista que contiene objetos con dos propiedades
+const scores = [
+  { name: "A", score: 95 },
+  { name: "L", score: 98 },
+  { name: "M", score: 80 },
+  { name: "E", score: 50 },
+  { name: "M", score: 85 },
+  { name: "J", score: 100 },
+];
+
+const scoresGreaterEighty = scores.filter((element) => element.score > 80);
+console.log("Ejemplo 8: Filtrando elementos por score");
+console.log(scoresGreaterEighty);
